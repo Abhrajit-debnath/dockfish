cb689b9f7a26b00f7e55baef3882dc0171343741
Abhrajit Debnath <debnath.abhrajit2005@gmail.com>
2026-05-06 20:33:04 +0530

    feat: add user options interactivity with options saving

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,30 @@
-console.log("hello")
+#!/usr/bin/env node
+
+import { program } from "commander";
+import dockerfileGenerator from "./commands/dockerFilegenerator.js";
+
+
+
+const initProgram = () => {
+    program.name('dockfish')
+        .description('CLI tool for generating Dockerfiles and learn docker')
+        .version("1.0.0")
+
+    program.command("start")
+        .description("Generate Dockerfile for your project").action(async () => {
+            dockerfileGenerator()
+
+        })
+
+    program.parse()
+
+}
+
+
+
+initProgram()
+
+
+
+
+
